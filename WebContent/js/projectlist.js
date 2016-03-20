0964a391cd90550a49ece1306f64a176ec7be197
EdStrickland <2499128545@qq.com>
2016-03-21 05:35:01 +0800

    encodeURI

--- WebContent/js/projectlist.js
+++ WebContent/js/projectlist.js
@@ -11,19 +11,7 @@ $(document).ready(function(){
         }
     });
     function Search(){
-        location.href ="projectlist.jsp?pages=1&s="+toUnicode($('#search-box-input').val());
-    }
-    function toUnicode(str) {
-      var temp,
-      i = 0,
-      r = '',
-      len = str.length;
-      for (; i < len; i++) {
-      temp = str.charCodeAt(i).toString(16);
-      while ( temp.length < 4 )
-      temp = '0' + temp;
-      r += '\\u' + temp;
-      };
-      return r;
+        var loc ="projectlist.jsp?pages=1&s="+$('#search-box-input').val();
+        location.href = encodeURI(loc);
     }
 })
