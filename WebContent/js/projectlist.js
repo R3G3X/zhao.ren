35626a973bbe96f21d5f0af7b423917408789909
EdStrickland <2499128545@qq.com>
2016-03-21 01:24:21 +0800

    search

--- WebContent/js/projectlist.js
+++ WebContent/js/projectlist.js
@@ -2,4 +2,15 @@ $(document).ready(function(){
 	$(".project-list-info").click(function(){
 		location.href ="../../html/project/project.jsp?id="+$(this).parent().attr("id");
 	})
+  $("#search-box-btn").click(function(){
+    $.post(getRootPath()+"/jsp/project_list.jsp",
+                {"pages":1,
+                "s":$("#psearch-box-input").val()},
+                function(data, status, xhr){
+                    alert("search complete");
+                })
+            .error(function(data,status,e){
+                alert("search incomplete");
+            })
+  })
 })
