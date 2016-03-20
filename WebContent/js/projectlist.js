6295585e7240dc24519d5ec212dbc65e5a870acf
EdStrickland <2499128545@qq.com>
2016-03-21 02:29:49 +0800

    search

--- WebContent/js/projectlist.js
+++ WebContent/js/projectlist.js
@@ -11,6 +11,19 @@ $(document).ready(function(){
         }
     });
     function Search(){
-        location.href ="projectlist.jsp?pages=1&s="+$('#search-box-input').val();
+        location.href ="projectlist.jsp?pages=1&s="+toUnicode($('#search-box-input').val());
+    }
+    function toUnicode(str) {
+      var temp,
+      i = 0,
+      r = '',
+      len = str.length;
+      for (; i < len; i++) {
+      temp = str.charCodeAt(i).toString(16);
+      while ( temp.length < 4 )
+      temp = '0' + temp;
+      r += '\\u' + temp;
+      };
+      return r;
     }
 })
