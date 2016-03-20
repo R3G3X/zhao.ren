bec65987e127ce49597d2288e17d7802cef2c25f
EdStrickland <2499128545@qq.com>
2016-03-20 20:27:32 +0800

    项目管理 个人信息 标签

--- WebContent/js/projectCreate.js
+++ WebContent/js/projectCreate.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+	var count = 0;
 	$("#project-create-post").click(function() {
 		$("#check-project-status").html("<font color='red'>添加中...</font>");
+		alert(count);
 		$.post(getRootPath() + "/jsp/doCreateProject.jsp", {
 			"name" : $("#projectname").val(),
 			"num" : $("#projectMenRequired").val(),
@@ -13,5 +15,14 @@ $(document).ready(function() {
 		}).error(function(data, status, e) {
 			$("#check-project-status").html("<font color='red'>添加失败！请检查信息是否有误</font>");
 		})
+	})
+	$(".lable").click(function(){
+		if($(this).attr("id")=="0"){
+			$(this).removeClass("label-default").addClass("label-primary").attr("id","1");
+			count += Number($(this).parent().attr("id"));
+		}else{
+			$(this).removeClass("label-primary").addClass("label-default").attr("id","0");
+			count -= Number($(this).parent().attr("id"));
+		}
 	})
 })
